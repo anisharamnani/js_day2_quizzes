d195ffaa12f9ecba96b311ce13d5ca56e8fbff97
anisharamnani <anisha.ramnani@gmail.com>
2014-01-21 23:25:48 -0500

    working and twerking dog solution

--- dog_years/src/DogYears.js
+++ dog_years/src/DogYears.js
@@ -1,2 +1,17 @@
 //create an object called dog
 //it should have 3 functions, ageInYears, ageInDays, ageInDogYears
+function Dog() {
+	this.age = Math.floor(Math.floor(Math.random()*10)+1);
+}
+
+Dog.prototype.ageInYears = function() {
+	return this.age; 
+};
+
+Dog.prototype.ageInDays = function() {
+	return this.age * 365;
+};
+
+Dog.prototype.ageInDogYears = function() {
+	return this.age * 7;
+};
